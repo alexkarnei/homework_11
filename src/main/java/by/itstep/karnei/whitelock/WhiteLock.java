8a062fc185717ce9f45899ad6128641cf2739c11
AlexKarnei <46269001+AlexKarnei@users.noreply.github.com>
2019-04-06 00:49:03 +0300

    initial commit

--- src/main/java/by/itstep/karnei/whitelock/WhiteLock.java
+++ src/main/java/by/itstep/karnei/whitelock/WhiteLock.java
@@ -1,23 +1,6 @@
 package by.itstep.karnei.whitelock;
 
-import by.itstep.karnei.FirstObject;
-import by.itstep.karnei.SecondObject;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class WhiteLock {
 
-    FirstObject firstObject = new FirstObject();
-    SecondObject secondObject = new SecondObject();
-    ExecutorService executorService;
-
-    private WhiteLock() {
-        executorService = Executors.newFixedThreadPool(2);
     }
-
-    public static void main(String[] args) {
-        new WhiteLock();
-
-    }
-}
